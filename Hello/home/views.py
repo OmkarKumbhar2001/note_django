37395c3a4e4bdbae402b0fc0800fed7feb3db6c5
Omkar Kumbhar <omkarkumbhar2001@gmail.com>
2023-03-26 17:17:18 +0530

    add stattic file

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Here is your home page")
+
+
+def about(request):
+    return HttpResponse("Here is your about page")
+
+
+def contact(request):
+    return HttpResponse("Here is your contact page")
+
+
+
+def services(request):
+    return HttpResponse("Here is your services page")
+
+
